5872ddb691a578d3ae4fdc281b60b6030c1d19ab
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-04-09 17:57:03 +0100

    Sorting var names

--- src/tree.py
+++ src/tree.py
@@ -4,11 +4,11 @@
 ''' Contains methods and attributes of the Tree class '''
 class Tree:
     ''' Creates tree '''
-    def __init__(self, branches=[1]):
-        self.branches = branches
+    def __init__(self, fib=[1]):
+        self.fib = fib
 
     def tick(self):
         '''Adds next num to the fib seq'''
-        if len(self.branches) > 1:
-            self.branches.append(self.branches[-1] + self.branches[-2])
-        else: self.branches.append(1)
+        if len(self.fib) > 1:
+            self.fib.append(self.fib[-1] + self.fib[-2])
+        else: self.fib.append(1)
