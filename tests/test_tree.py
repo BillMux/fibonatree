adff3dabcb2f6dc75adc553c97b963d1591c07a5
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-04-09 16:22:59 +0100

    New tree spec, fib arr implemented

---
+++ tests/test_tree.py
@@ -0,0 +1,20 @@
+import pytest
+from src.tree import Tree
+
+def setup():
+    global tree
+    tree = Tree()
+
+def test_start_seq_from_one():
+    assert tree.branches == [1]
+
+def test_first_tick_adds_one():
+    tree.first_tick()
+    assert tree.branches == [1, 1]
+
+def setup_after_first_tick():
+    tree = Tree([1, 1])
+
+def test_following_ticks_add_fib_nums():
+    for i in range(5): tree.tick()
+    assert tree.branches == [1, 1, 2, 3, 5, 8, 13]
