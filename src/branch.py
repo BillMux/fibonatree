cb759403b44e2bb7b37ff3f55eedef0d30b9a553
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-04-09 15:05:47 +0100

    First commit: Set up env, init first class

---
+++ src/branch.py
@@ -0,0 +1,3 @@
+class Branch:
+    def __init__(self, is_split):
+        self.is_split = is_split
---
+++ tests/test_branch.py
@@ -0,0 +1,9 @@
+import pytest
+from src.branch import Branch
+
+def setup():
+    global branch
+    branch = Branch(False)
+
+def test_starts_with_bool_split_attr():
+    assert branch.is_split == False
