f2d135578ee492be7874e9866cf3e8efeb41da4e
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-04-11 17:07:09 +0100

    Implementing second gen growth, test fails

--- src/branch.py
+++ src/branch.py
@@ -4,9 +4,12 @@
 ''' Contains methods and attributes of the Branch class '''
 class Branch:
     ''' Creates branch '''
-    def __init__(self, is_split=False):
+    def __init__(self, previous_branches=None, is_split=False):
         self.is_split = is_split
         self.growth = []
 
     def generate(self):
-        self.growth.append(self)
+        if len(self.growth) > 0:
+            self.growth[0].growth.extend([Branch(), Branch()])
+        else:
+            self.growth.append(Branch())
